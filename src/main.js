d94eafa902e23ba4301491edd99db14393e04a46
En1Li <1401207141@qq.com>
2022-06-28 19:45:32 +0800

    完成了reduce求个数

--- src/main.js
+++ src/main.js
@@ -13,3 +13,15 @@ const sum = arr.reduce((res , item)=>{
   return res += item.a
 } , 0)
 console.log(sum);
+
+let str = 'asdfsdadasgdsasdasdfa'
+const arr1 = str.split('')
+const obj = arr1.reduce((res , item)=>{
+  if(res[item]){
+    res[item]++
+  }else{
+    res[item] = 1
+  }
+  return res
+} , {})
+console.log(obj);
