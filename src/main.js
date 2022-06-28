f89641df077c3fe1bd3a3f76c49713915db7b36b
En1Li <1401207141@qq.com>
2022-06-28 19:35:11 +0800

    完成了reduce求和

--- src/main.js
+++ src/main.js
@@ -6,3 +6,10 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+let arr = [{a:100}, {a: 20}, {a:-10}]
+const sum = arr.reduce((res , item)=>{
+  return res += item.a
+} , 0)
+console.log(sum);
