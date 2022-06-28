5c1c6c41422f3badd33f94330ca718b43ac66201
En1Li <1401207141@qq.com>
2022-06-28 20:10:17 +0800

    完成了翻转世界案例

---
+++ src/1.翻转.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <button @click="fn">翻转世界</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg:'Hello , Vue'
+    }
+  },
+  methods: {
+    fn(){
+      this.msg = this.msg.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
